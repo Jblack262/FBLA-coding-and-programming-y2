06cf6be7738b1bf830897619e3cf65cb06ed562e
Jblack262 <71471784+Jblack262@users.noreply.github.com>
2021-09-13 17:04:28 -0700

    started working on suggestions functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import React from 'react';
 import LocationSearchInput from './components/PlacesInput'
-import Map from './components/Map'
-import SavedLocations from './components/SavedLocations'
+// import Map from './components/Map'
+// import SavedLocations from './components/SavedLocations'
 
 function App() {
-	const [location, setLocation] = React.useState({lat: 33.6362399, lng: -112.1344286})
-	const [address, setAddress] = React.useState('')
-	const [savedLocations, setSavedLocations] = React.useState([]);
-	const [zoom, setZoom] = React.useState(2);
+	// const [location, setLocation] = React.useState({lat: 33.6362399, lng: -112.1344286})
+	// const [address, setAddress] = React.useState('')
+	const [title, setTitle] = React.useState('')
+	// const [savedLocations, setSavedLocations] = React.useState([]);
+	// const [zoom, setZoom] = React.useState(2);
 
-	const onSelectLocation = (location, address) => {
-		setAddress(address);
-		setLocation(location);
+	// const onSelectLocation = (location, address) => {
+	// 	setAddress(address);
+	// 	setLocation(location);
 
-		const addressLength = address.split(",").length;
-		(addressLength >= 1 && addressLength <= 3) ? setZoom(addressLength + 3) : setZoom(15)//sets zoom based on number of elements in address
-	}
+	// 	const addressLength = address.split(",").length;
+	// 	(addressLength >= 1 && addressLength <= 3) ? setZoom(addressLength + 3) : setZoom(15)//sets zoom based on number of elements in address
+	// }
 
-	const onSetAddress = (address)
+	// const keepLocation = () => {
+	// 	console.log(location);
+	// 	if (address !== '' && !([...savedLocations].includes(address))) setSavedLocations([...savedLocations, address]); //filters out empty searches and duplicate searches
+	// }
 
-	const keepLocation = () => {
-		if (address !== '' && !([...savedLocations].includes(address))) setSavedLocations([...savedLocations, address]); //filters out empty searches and duplicate searches
-	}
+	// const clearLocation = () => {
+	// 	setAddress('');
+	// 	setZoom(2);
+	// }
 
-	const clearLocation = () => {
-		setAddress('');
-		setZoom(2);
+	const newTitle = (title) => {
+		console.log(title)
+		setTitle(title);
 	}
 
   return (
     <div className="App">
-		<LocationSearchInput clearLocation={clearLocation} onSelectLocation={onSelectLocation}/>
-		<h1>{address}</h1>
-		<div className="container">
-			<Map zoom={zoom} location={location} onSelectLocation={onSelectLocation} keepLocation={keepLocation}/>
-			<SavedLocations locations={savedLocations}/>
-		</div>
+			<LocationSearchInput newTitle={newTitle} /* clearLocation={clearLocation} onSelectLocation={onSelectLocation} *//>
+			<h1>{title}</h1>
+			{/* <div className="container">
+				<Map zoom={zoom} location={location} onSelectLocation={onSelectLocation} keepLocation={keepLocation}/>
+				<SavedLocations locations={savedLocations}/>
+			</div> */}
     </div>
   );
 }
--- src/components/PlacesInput.js
+++ src/components/PlacesInput.js
@@ -6,58 +6,54 @@ import PlacesAutocomplete, {
 
 import {IoCloseSharp} from 'react-icons/io5'
 
-function PlacesInput({onSelectLocation, clearLocation}) {
-    const [dumbAddress, setDumbAddress] = React.useState('');
+function PlacesInput(/*{ onSelectLocation, clearLocation,  newTitle}*/) {
     const [address, setAddress] = React.useState('');
+    const [suggestions, setSuggestions] = React.useState('');
 
     const handleChange = (address) => {
         setAddress(address)
     }
 
-
-
     const clearSearch = () => {
         setAddress('');
-        clearLocation();
+        // clearLocation();
     }
-
-    const test = (e, suggestions) => {
-        if (e.key === "Enter" && suggestions[0]) {
-            setAddress(suggestions[0].description);
-            // console.log(suggestions)
-        }
-    }
-    const handleSelect = (address ) => {
-        setDumbAddress(address)
+    
+    const handleSelect = (address) => {
+        console.log(suggestions);
+        // newTitle(firstSuggestion);
         geocodeByAddress(address)
           .then(results => getLatLng(results[0]))
-          .then(latLng => onSelectLocation(latLng, address))
+        //   .then(latLng => console.log(latLng, address))
           .catch(error => console.error('Error', error));
     }
     return (
-        <PlacesAutocomplete onChange={handleChange} value={dumbAddress} onSelect={handleSelect}>
-            {({ getInputProps, suggestions, getSuggestionItemProps, loading}) => (
-                
+        
+    <PlacesAutocomplete value={address} onChange={handleChange} onSelect={handleSelect}>
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => {
+            console.log(suggestions);
+            // if (address === '' ) setSuggestions(suggestions)
+            return (
                 <div className="Search">
-                    <div className="input" onKeyDown={(e)=>{test(e, suggestions)}}>
+                    <div className="input">
                         <input {...getInputProps({ placeholder: 'Search Places ...', className: 'location-search-input', })} />
                         <button onClick={clearSearch}><IoCloseSharp/></button>
                     </div>
                     <div className="suggestionsContainer">
-
                         {loading && <div>Loading...</div>}
-
-                        {suggestions.map((suggestion, index) => {
+                        {suggestions.map(suggestion => {
+                            const {placeId, description} = suggestion;
                             return (
-                                <div key={index} {...getSuggestionItemProps(suggestion)} className="suggestion-item">
-                                    <span>{suggestion.description}</span>
+                                <div key={placeId} className="suggestion-item" {...getSuggestionItemProps(suggestion)} >
+                                    <span>{description}</span>
                                 </div>
                             );
                         })}
                     </div>
                 </div>
-            )}
-        </PlacesAutocomplete>
+            )
+        }}
+    </PlacesAutocomplete>
     )
 }
 
