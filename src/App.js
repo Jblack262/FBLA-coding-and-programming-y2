d55c9bfa5a367120f673f10254a471198ca76b8c
JustinMelore <kingnihilusjt@gmail.com>
2021-12-22 14:22:11 -0700

    Made a placeholder layout for the search page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LocationSearchInput from './components/PlacesInput'
+import Header from './components/Header';
 // import Map from './components/Map'
 // import SavedLocations from './components/SavedLocations'
 
@@ -34,13 +35,26 @@ function App() {
 	}
 
   return (
-    <div className="App">
+	<div className="App">
+		<Header></Header>
+		<p id='searchDescription'>Search for various attractions, either by name or using the filters below</p>
 			<LocationSearchInput newTitle={newTitle} /* clearLocation={clearLocation} onSelectLocation={onSelectLocation} *//>
 			<h1>{title}</h1>
 			{/* <div className="container">
 				<Map zoom={zoom} location={location} onSelectLocation={onSelectLocation} keepLocation={keepLocation}/>
 				<SavedLocations locations={savedLocations}/>
 			</div> */}
+		<div id="filters">
+			<h1>Filters</h1>
+			<ul>
+				<li><p>Filter 1</p></li>
+				<li><p>Filter 2</p></li>
+				<li><p>Filter 3</p></li>
+				<li><p>Filter 4</p></li>
+				<li><p>Filter 5</p></li>
+			</ul>
+		</div>
+		<button>Search</button>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+//Header that can be reused on all pages; includes the company name and logo
+const Header = () => {
+    return (
+        <header>
+            PLACEHOLDER
+        </header>
+    )
+}
+
+export default Header
