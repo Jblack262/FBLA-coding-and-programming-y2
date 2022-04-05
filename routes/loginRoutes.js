0152527c47830e7ac91de5c5eae6e498b30f0c35
JD Blackman <JDBlackman10@gmail.com>
2022-04-05 09:16:28 -0700

    testing temporary create account fix

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -45,7 +45,7 @@ app.post('/', async (req, res) => { //create user
                                 .then((value) => {
                                     console.log(value)
                                     // req.flash('success_msg', 'You have now registered')
-                                    res.redirect('/login');
+                                    // res.redirect('/login');
                                     res.sendStatus(200)
                                 })
                                 .catch(value => console.log(value))
